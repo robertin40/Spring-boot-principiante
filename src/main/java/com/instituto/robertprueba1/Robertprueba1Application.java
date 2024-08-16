5b0a8662afc0a441dd6e8efd081b90e8c3490520
ROBERTXD <ROBERTXD@host.docker.internal>
2024-08-16 17:12:43 -0500

    mi primer spring boot page

---
+++ src/main/java/com/instituto/robertprueba1/Robertprueba1Application.java
@@ -0,0 +1,13 @@
+package com.instituto.robertprueba1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Robertprueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Robertprueba1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/instituto/robertprueba1/Robertprueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instituto.robertprueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Robertprueba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
